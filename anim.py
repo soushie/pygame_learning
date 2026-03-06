aaf77a4460cf2e02f4626514538f1b29b6d75fec
soushie <holypotato77@gmail.com>
2026-03-07 00:07:03 +0400

    Add files via upload

---
+++ anim.py
@@ -0,0 +1,93 @@
+import pygame
+from classes import SpriteSheet
+
+pygame.init()
+
+#screen measurements
+Screen_Width = 1440
+Screen_Height = 720
+
+
+#some random variables
+BG = (50, 50, 50)
+Black = (0, 0, 0)
+
+player_pos =  pygame.Vector2(Screen_Width/2,Screen_Height/2)
+speed = 100
+clock= pygame.time.Clock()
+dt=0
+
+#sets screen
+screen= pygame.display.set_mode((Screen_Width , Screen_Height))
+pygame.display.set_caption("Spritesheets")
+
+#gets sprites for character and animation
+sprite_sheet_image = pygame.image.load("assets/images/doux.png").convert_alpha()
+sprite_sheet = SpriteSheet(sprite_sheet_image)
+
+#create animation list
+animation_list = []
+animation_steps = [4, 6, 3, 4, 7] #animations places in sheet such as running, jumping etc.
+action = 0 #what is player doing (stayin, jumping, etc.)
+last_update = pygame.time.get_ticks()
+animation_cooldown = 100
+frame = 0
+step_counter = 0
+
+for animation in animation_steps:
+    temp_img_list = []
+    for _ in range(animation):
+        temp_img_list.append(sprite_sheet.get_image(step_counter, 24, 24, 3, Black))
+        step_counter += 1
+    animation_list.append(temp_img_list)
+
+
+#gameloop
+running = True
+
+while running:
+
+    screen.fill(BG)
+
+    #update animation
+    current_time = pygame.time.get_ticks()
+    if current_time - last_update >= animation_cooldown:
+        frame += 1
+        last_update = current_time
+
+        if frame == animation_steps[action]:
+            frame = 0
+
+    screen.blit(animation_list[action][frame], player_pos)
+
+    pygame.display.update()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN and action >0:
+                action -=1
+                frame = 0
+            if event.key == pygame.K_UP and action < len(animation_list)-1:
+                action +=1
+                frame = 0
+    
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= speed * dt
+        action = 1
+    if keys[pygame.K_s]:
+        player_pos.y += speed * dt
+        action = 1
+    if keys[pygame.K_a]:
+        player_pos.x -= speed *dt
+        action = 1
+    if keys[pygame.K_d]:
+        player_pos.x += speed * dt
+        action = 1
+
+    dt = clock.tick(60) / 1000
+            
+
+pygame.quit()
