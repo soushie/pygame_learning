cf515cb5afe84140b08419aa4565c7b89fa5a444
soushie <holypotato77@gmail.com>
2026-03-28 20:30:11 +0400

    Add files via upload

--- anim.py
+++ anim.py
@@ -19,15 +19,21 @@ dt=0
 
 #sets screen
 screen= pygame.display.set_mode((Screen_Width , Screen_Height))
-pygame.display.set_caption("Spritesheets")
+pygame.display.set_caption("dinosawr")
 
 #gets sprites for character and animation
 sprite_sheet_image = pygame.image.load("assets/images/doux.png").convert_alpha()
 sprite_sheet = SpriteSheet(sprite_sheet_image)
 
+#door animation
+door_sheet = SpriteSheet(pygame.image.load("assets/images/door.png").convert_alpha())
+door_anim = [door_sheet.get_image(0, 24, 24, 4, Black) ,door_sheet.get_image(1, 24, 24, 4, Black) ]
+door_state = 0
+f = 1
+
 #create animation list
 animation_list = []
-animation_steps = [4, 6, 4, 6, 3, 4] #animations places in sheet such as running, jumping etc.
+animation_steps = [4, 6, 4, 6] #animations places in sheet such as running, jumping etc.
 action = 0 #what is player doing (stayin, jumping, etc.)
 last_update = pygame.time.get_ticks()
 animation_cooldown = 100
@@ -58,28 +64,37 @@ while running:
         if frame >= animation_steps[action]:
             frame = 0
 
-    screen.blit(animation_list[action][frame], player_pos)
+    screen.blit(door_anim[door_state], (Screen_Width/2,Screen_Height/2))
 
+    screen.blit(animation_list[action][frame], player_pos)
+    
     pygame.display.update()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        #if event.type == pygame.KEYDOWN:
-        #    if event.key == pygame.K_DOWN and action >0:
-        #        action -=1
-        #        frame = 0
-        #    if event.key == pygame.K_UP and action < len(animation_list)-1:
-        #        action +=1
-        #        frame = 0
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_e:
+                door_state += f
+                f = -f
+
     moving = False
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
         player_pos.y -= speed * dt
+        if action == 2:
+            action = 3
+        if action == 0:
+            action = 1
         moving = True
     if keys[pygame.K_s]:
         player_pos.y += speed * dt
+        if action == 0:
+            action = 1
+        if action == 2:
+            action = 3
         moving = True
     if keys[pygame.K_a]:
         player_pos.x -= speed *dt
@@ -90,8 +105,9 @@ while running:
         action = 3
         moving = True
 
+
     if moving == False :
-        if action == 1:
+        if action == 1 :
             action = 0
         if action == 3:
             action = 2
